3d8d35bf4c408794974b188ecdfaf2ce019d5396
congjj <congjiajia_007@hotmail.com>
2017-12-09 20:45:48 +0800

    Initial commit

---
+++ app/src/main/java/libs/congjiajia/shenyang/ln/cn/qrcode/MainActivity.java
@@ -0,0 +1,15 @@
+package libs.congjiajia.shenyang.ln.cn.qrcode;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
